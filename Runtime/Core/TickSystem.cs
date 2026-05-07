3d62618de54267594792e897720e3b8b27da4e3f
Helvest <master.dominice@gmail.com>
2026-05-07 08:47:40 +0200

    up

--- Runtime/Core/TickSystem.cs
+++ Runtime/Core/TickSystem.cs
@@ -3,168 +3,168 @@ using System.Collections.Generic;
 
 namespace TickPhysics
 {
-    public abstract class TickSystem : ITickSystem
-    {
-        #region Fields
+public abstract class TickSystem : ITickSystem
+{
+#region Fields
 
-        public virtual bool IsPhysicUpdated { get; set; } = true;
+	public virtual bool IsPhysicUpdated { get; set; } = true;
 
-        public virtual double TimeAtSimulation { get; protected set; }
+	public virtual double TimeAtSimulation { get; protected set; }
 
-        public virtual double NormalTime { get; protected set; }
+	public virtual double NormalTime { get; protected set; }
 
-        public virtual double FixedTime { get; protected set; }
+	public virtual double FixedTime { get; protected set; }
 
-        public virtual uint FixedFrameCount { get; protected set; }
+	public virtual uint FixedFrameCount { get; protected set; }
 
-        public virtual float ExtraDeltaTime { get; protected set; }
+	public virtual float ExtraDeltaTime { get; protected set; }
 
-        protected virtual bool IsInUpdateLoop { get; set; }
+	protected virtual bool IsInUpdateLoop { get; set; }
 
-        protected virtual int LoopIndex { get; set; }
+	protected virtual int LoopIndex { get; set; }
 
-        #endregion
+#endregion
 
-        #region Events
+#region Events
 
-        public event Action EventReadInput;
-        public event Action EventUpdatePhysic;
-        public event Action<uint> EventProcessInput;
-        public event Action EventUpdateGraphic;
+	public event Action EventReadInput;
+	public event Action EventUpdatePhysic;
+	public event Action<uint> EventProcessInput;
+	public event Action EventUpdateGraphic;
 
-        #endregion
+#endregion
 
-        #region IPhysicObject
+#region IPhysicObject
 
-        private readonly List<IPhysicsObject> _physicObjects = new List<IPhysicsObject>(256);
+	private readonly List<IPhysicsObject> _physicObjects = new(256);
 
-        public virtual void Add(params IPhysicsObject[] physicObjectsToAdd)
-        {
-            foreach (var item in physicObjectsToAdd)
-            {
-                if (!_physicObjects.Contains(item))
-                {
-                    _physicObjects.Add(item);
-                }
-            }
-        }
+	public virtual void Add(params IPhysicsObject[] physicObjectsToAdd)
+	{
+		foreach (var item in physicObjectsToAdd)
+		{
+			if (!_physicObjects.Contains(item))
+			{
+				_physicObjects.Add(item);
+			}
+		}
+	}
 
-        public virtual void Remove(params IPhysicsObject[] physicObjectsToRemove)
-        {
-            foreach (var item in physicObjectsToRemove)
-            {
-                int index = _physicObjects.IndexOf(item);
-                _physicObjects.RemoveAt(index);
+	public virtual void Remove(params IPhysicsObject[] physicObjectsToRemove)
+	{
+		foreach (var item in physicObjectsToRemove)
+		{
+			int index = _physicObjects.IndexOf(item);
+			_physicObjects.RemoveAt(index);
 
-                if (IsInUpdateLoop && index < LoopIndex)
-                {
-                    LoopIndex--;
-                }
-            }
-        }
+			if (IsInUpdateLoop && index < LoopIndex)
+			{
+				LoopIndex--;
+			}
+		}
+	}
 
-        #endregion
+#endregion
 
-        #region Tick
+#region Tick
 
-        public virtual void Tick(double time, double deltaTime, double fixedDeltaTime)
-        {
-            ReadInput();
+	public virtual void Tick(double time, double deltaTime, double fixedDeltaTime)
+	{
+		ReadInput();
 
-            if (!IsPhysicUpdated)
-            {
-                return;
-            }
+		if (!IsPhysicUpdated)
+		{
+			return;
+		}
 
-            TimeAtSimulation = time;
+		TimeAtSimulation = time;
 
-            NormalTime += deltaTime;
+		NormalTime += deltaTime;
 
-            PhysicTick(fixedDeltaTime);
+		PhysicTick(fixedDeltaTime);
 
-            CalculateExtraDeltaTime();
+		CalculateExtraDeltaTime();
 
-            // Here you can access the transforms state right after the simulation, if needed...	
-            UpdateGraphic();
+		// Here you can access the transforms state right after the simulation, if needed...	
+		UpdateGraphic();
 
-            //IsInUpdateLoop = false;
-        }
+		//IsInUpdateLoop = false;
+	}
 
-        protected virtual void PhysicTick(double fixedDeltaTime)
-        {
-            if (fixedDeltaTime <= 0)
-            {
-                return;
-            }
+	protected virtual void PhysicTick(double fixedDeltaTime)
+	{
+		if (fixedDeltaTime <= 0)
+		{
+			return;
+		}
 
-            // Catch up with the game time.
-            // Advance the physics simulation by portions of fixedDeltaTime
-            while (NormalTime >= FixedTime + fixedDeltaTime)
-            {
-                FixedFrameCount++;
+		// Catch up with the game time.
+		// Advance the physics simulation by portions of fixedDeltaTime
+		while (NormalTime >= FixedTime + fixedDeltaTime)
+		{
+			FixedFrameCount++;
 
-                FixedTime += fixedDeltaTime;
+			FixedTime += fixedDeltaTime;
 
-                //Prepare inputs for actual physic frame
-                ProcessInput();
+			//Prepare inputs for actual physic frame
+			ProcessInput();
 
-                //A custom FixedUpdate
-                UpdatePhysic();
+			//A custom FixedUpdate
+			UpdatePhysic();
 
-                //Advance the simulation, this will also call OnTrigger and OnCollider
-                SimulatePhysic(fixedDeltaTime);
-            }
-        }
+			//Advance the simulation, this will also call OnTrigger and OnCollider
+			SimulatePhysic(fixedDeltaTime);
+		}
+	}
 
-        protected virtual void CalculateExtraDeltaTime()
-        {
-            ExtraDeltaTime = (float)(NormalTime - FixedTime);
-        }
+	protected virtual void CalculateExtraDeltaTime()
+	{
+		ExtraDeltaTime = (float)(NormalTime - FixedTime);
+	}
 
-        #endregion
+#endregion
 
-        #region Custom Updates
+#region Custom Updates
 
-        protected virtual void ReadInput()
-        {
-            EventReadInput?.Invoke();
-        }
+	protected virtual void ReadInput()
+	{
+		EventReadInput?.Invoke();
+	}
 
-        protected virtual void UpdatePhysic()
-        {
-            IsInUpdateLoop = true;
+	protected virtual void UpdatePhysic()
+	{
+		IsInUpdateLoop = true;
 
-            for (LoopIndex = 0; LoopIndex < _physicObjects.Count; LoopIndex++)
-            {
-                _physicObjects[LoopIndex].UpdatePhysics();
-            }
+		for (LoopIndex = 0; LoopIndex < _physicObjects.Count; LoopIndex++)
+		{
+			_physicObjects[LoopIndex].UpdatePhysics();
+		}
 
-            IsInUpdateLoop = false;
+		IsInUpdateLoop = false;
 
-            EventUpdatePhysic?.Invoke();
-        }
+		EventUpdatePhysic?.Invoke();
+	}
 
-        protected virtual void ProcessInput()
-        {
-            EventProcessInput?.Invoke(FixedFrameCount);
-        }
+	protected virtual void ProcessInput()
+	{
+		EventProcessInput?.Invoke(FixedFrameCount);
+	}
 
-        protected abstract void SimulatePhysic(double fixedDeltaTime);
+	protected abstract void SimulatePhysic(double fixedDeltaTime);
 
-        protected virtual void UpdateGraphic()
-        {
-            IsInUpdateLoop = true;
+	protected virtual void UpdateGraphic()
+	{
+		IsInUpdateLoop = true;
 
-            for (LoopIndex = 0; LoopIndex < _physicObjects.Count; LoopIndex++)
-            {
-                _physicObjects[LoopIndex].UpdateGraphics();
-            }
+		for (LoopIndex = 0; LoopIndex < _physicObjects.Count; LoopIndex++)
+		{
+			_physicObjects[LoopIndex].UpdateGraphics();
+		}
 
-            IsInUpdateLoop = false;
+		IsInUpdateLoop = false;
 
-            EventUpdateGraphic?.Invoke();
-        }
+		EventUpdateGraphic?.Invoke();
+	}
 
-        #endregion
-    }
+#endregion
+}
 }
